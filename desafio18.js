a667f034d15ff3e84ae1bb51b3690154d0e5dc87
Kevin110706 <159440846+Kevin110706@users.noreply.github.com>
2024-09-26 10:58:39 -0300

    Add files via upload

--- desafio18.js
+++ desafio18.js
@@ -48,3 +48,13 @@ function setErrorFor(input, message) {
     formControl.className = "form-control error";
 }
 
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
